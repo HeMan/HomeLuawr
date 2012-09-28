bf202c38fba0867e23be0010739bbdd943504144
Jimmy Hedman <jimmy.hedman@southpole.se>
2012-09-29 00:31:13 +0200

    Rewrote it to use poll
    - Instead of blocking on read it now uses poll from nixio.
    - Initialises the rfxcom.

--- read.lua
+++ read.lua
@@ -16,11 +16,45 @@
 --
 
 local parse=require "parse"
+local encode = require "encode"
+local nixio = require "nixio"
+local os = require "os"
 
-len = io.read(1)
-data = io.read(string.byte(len))
+tty = assert(nixio.open(arg[1],"r+"))
 
-realdata=parse.parse(data)
-for s,c in pairs(realdata) do
-	print(s,c)
+print("reseting")
+io.write(encode.encode(encode.reset()))
+tty:write(encode.encode(encode.reset()))
+nixio.nanosleep(0,500000000)
+
+while not tty:read(0) do
+	tty:read(1)
 end
+
+io.write(encode.encode(encode.get_status()))
+tty:write(encode.encode(encode.get_status()))
+
+local ttypoll = {
+	{ fd=tty, events=nixio.poll_flags("in") }
+}
+
+repeat
+	repeat
+		print("polling")
+		local stat, code = nixio.poll(ttypoll, 10000)
+	until stat and stat > 0
+
+	len = tty:read(1)
+	print("read num"..string.byte(len))
+	data = tty:read(string.byte(len))
+
+	realdata=parse.parse(data)
+	if realdata then
+		for s,c in pairs(realdata) do
+			print(s,c)
+		end
+	else
+		print("Unknown 0x"..string.format("%x",string.byte(data:sub(1,1))))
+	end
+
+until false
