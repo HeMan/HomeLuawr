719c498f2605f5757aebb466529a1d2bc32b50db
Jimmy Hedman <jimmy.hedman@southpole.se>
2013-03-13 20:54:51 +0100

    Added some callbacks.
    - It now logs to sen.se and cosm.
    - Adds data to RRD graph.

--- callbacks.lua
+++ callbacks.lua
@@ -23,7 +23,41 @@ function parsedata ( data  )
     end
 end  ----------  end of function parsedata  ----------
 
+function tempwrite ( data )
+  if data.id == 40192 then
+    local fil = io.open("/www/rfxcom/ut.html","w")
+    fil:write(data.temp)
+    fil:close()
+  end
+	return true
+end  ----------  end of function tempwrite  ----------
+
+function temprrd ( data )
+  if data.id == 40192 then
+    io.popen("/usb/usr/bin/rrdupdate /usb/proj/data/temperature.rrd N:"..data.temp)
+  end
+	return true
+end  ----------  end of function temprrd  ----------
+
+
+function tempcosm ( data )
+  if data.id == 40192 then
+    io.popen("curl --silent --request PUT --data-binary '{ \"version\":\"1.0.0\", \"datastreams\":[{\"id\":\"Utetemp\", \"current_value\":\""..data.temp.."\"}]}' --header \"X-ApiKey: 9422WoQJ_ntMtDznZ0aVyi-KA0mSAKxOZlV6QUxIUHYyUT0g\" -o /dev/null http://api.cosm.com/v2/feeds/91433")
+  end
+	return true
+end  ----------  end of function tempcosm  ----------
+
+function tempsense ( data )
+  if data.id == 40192 then
+    io.popen("curl --silent --request POST -H \"sense_key: 2oGGutVpxpSQMEEl-jILOg\" -H \"Content-Type: application/json\" --data-binary '{\"feed_id\": 22946, \"value\": \""..data.temp.."\"}' http://api.sen.se/events/")
+  end
+	return true
+end  ----------  end of function tempsense  ----------
 pubsub:subscribe("SENSOR", parsedata)
+pubsub:subscribe("SENSOR", tempwrite)
+pubsub:subscribe("SENSOR", temprrd)
+pubsub:subscribe("SENSOR", tempcosm)
+pubsub:subscribe("SENSOR", tempsense)
 
 pubsub:subscribe("TIME", function(data) if (data.sec==0) then print(os.date("%c",os.time(data))) end end)
 
