a485f243f75ad34cc179e00a3a79acb3203b55c6
Jimmy Hedman <jimmy.hedman@southpole.se>
2013-03-13 22:06:13 +0100

    Simple testscript.
    - Sets the serial port with stty for binary data.

---
+++ prov.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash 
+#===============================================================================
+#
+#          FILE:  prov.sh
+# 
+#         USAGE:  ./prov.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Jimmy Hedman (jimmy), jimmy.hedman@southpole.se
+#       COMPANY: South Pole AB
+#       CREATED: 2012-09-17 21:56:11 CEST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+BAUD=38400
+RFXCOMTTY=/dev/ttyUSB0
+if [ $# -eq 1 ]; then
+	RFXCOMTTY=$1
+fi
+
+
+#exec < $RFXCOMTTY
+stty raw $BAUD < $RFXCOMTTY
+stty -a < $RFXCOMTTY
+lua ./server.lua 
